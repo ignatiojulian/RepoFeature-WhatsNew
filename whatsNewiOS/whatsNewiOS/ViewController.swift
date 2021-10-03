03139f98c6f45017a7bb1d6a217fc27609cc4bd9
Ignatio <ignatio.tara@multipolar.com>
2021-10-03 21:08:53 +0800

    add detail button

--- whatsNewiOS/whatsNewiOS/ViewController.swift
+++ whatsNewiOS/whatsNewiOS/ViewController.swift
@@ -30,18 +30,40 @@ class ViewController: UIViewController {
         // configuration.completionButton.backgroundColor = .orange
         configuration.apply(theme: .darkBlue)
         
+        let detailButton = WhatsNewViewController.DetailButton(
+            title: "Baca lebih lanjut",
+            action: .website(url: "https://github.com/SvenTiigi/WhatsNewKit")
+        )
+        configuration.detailButton = detailButton
+        
+        
+        configuration.titleView.secondaryColor = .init(
+            // The start index
+            startIndex: 36,
+            // The length of characters
+            length: 3,
+            // The secondary color to apply
+            color: .whatsNewKitLightBlue
+        )
+        
         /// Setup Custom Completion Button
-        configuration.completionButton.title = "Selanjutnya"
+        
+        let completionButton = WhatsNewViewController.CompletionButton(
+            title: "Selanjutnya",
+            action: .dismiss
+        )
+        configuration.completionButton = completionButton
         
         /// Setup Custom Layout & Animation
-        configuration.itemsView.layout = .centered
-        configuration.itemsView.animation = .slideRight
+//        configuration.itemsView.layout = .centered
+//        configuration.itemsView.animation = .slideRight
         
         
         let items = WhatsNew(title: "Hai, Apa yang terbaru dari Aplikasi BBA",
                              items: [
                                 WhatsNew.Item(title: "Fitur Favorite", subtitle: "Sekarang kamu bisa bertransaksi dari fitur favorit", image: UIImage(systemName: "heart.text.square.fill")),
-                                WhatsNew.Item(title: "QRIS", subtitle: "Sekarang QRIS sudah support di Mobile Banking BBA lho", image: UIImage(systemName: "camera.circle"))
+                                WhatsNew.Item(title: "QRIS", subtitle: "Sekarang QRIS sudah support di Mobile Banking BBA lho", image: UIImage(systemName: "camera.circle")),
+                                WhatsNew.Item(title: "Pocket Together", subtitle: "Yuhu, sekarang bisa nabung bareng", image: UIImage(systemName: "camera.circle"))
                              ]
         )
         
